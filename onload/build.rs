16911879a6b25b1cf0b8f16dbdef2702e0e47023
somewheve <somewheve@gmail.com>
2025-03-27 01:25:04 +0800

    update build.rs and do not support windows yet

--- onload/build.rs
+++ onload/build.rs
@@ -1,28 +1,34 @@
 #[cfg(feature = "debug")]
 extern crate bindgen;
 #[cfg(feature = "debug")]
+use std::fs::OpenOptions;
+#[cfg(feature = "debug")]
+use std::io::Write;
+#[cfg(feature = "debug")]
 use std::path::PathBuf;
-
 #[cfg(feature = "debug")]
 fn generate_binding() {
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result or panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("Unable to generate bindings")
+        .to_string();
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("bindings.rs");
+    println!("{}", out_path.display());
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .write(true)
+        .open(&out_path)
+        .expect("can't open bindings file");
+    file.write(b"#[cfg(windows)]\ncompile_error!(\"onload does not support windows platform\");")
+        .unwrap();
+    file.write(bindings.as_bytes()).unwrap();
 }
 
 fn main() {
     #[cfg(feature = "debug")]
     generate_binding();
-
     // link library
     println!("cargo:rustc-link-lib=onload_zf");
     println!("cargo:rustc-link-lib=ciul1");
